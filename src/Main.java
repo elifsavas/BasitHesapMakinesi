80e77a6bd39d4dc6470e8102a1ea400fc0cca58d
Elif Savaş <98321352+elifsavas@users.noreply.github.com>
2022-10-24 14:54:36 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,44 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //Kullanicidan 2 deger alacagiz ve bu degerlere top,çkr,çrp,bol komutları ekleyecegiz
+
+        int n1,n2, select ;
+        Scanner input = new Scanner(System.in) ;
+
+        System.out.print("Birinci Sayıyı Giriniz: ");
+        n1 = input.nextInt();
+
+        System.out.print("İkinci Sayıyı Giriniz: ");
+        n2 = input.nextInt();
+
+        System.out.println("1-Toplama\n2-Çıkarma\n3-Çarpma\n4-Bölme");
+        System.out.print("Yapmak İstediğiniz İşlem:");
+        select = input.nextInt();
+
+        switch (select) {
+            case 1 :
+                System.out.println("Sonuç:" + (n1+n2));
+                break;
+            case 2 :
+                System.out.println("Sonuç:" + (n1-n2));
+                break;
+            case 3 :
+                System.out.println("Sonuç:" + (n1*n2));
+                break;
+            case 4 :
+                if (n2 !=0) {
+                    System.out.println("Sonuç:" + (n1/n2));
+                } else {
+                    System.out.println("Hiçbir sayı 0'a bölünemez!");
+                }
+                break;
+            default:
+                System.out.println("Hatalı Giriş Yaptınız!");
+        }
+
+
     }
 }
